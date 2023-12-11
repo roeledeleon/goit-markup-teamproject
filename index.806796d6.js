9ff02fe8c2548ec6ed3dc88b471202859c412323
roeledeleon <roel.edeleon@gmail.com>
2023-12-11 11:21:15 +0000

    Deploying to gh-pages from @ roeledeleon/goit-markup-teamproject@f0517587f4326f4fdb309001011fccbb67cd8c92 🚀

---
+++ index.806796d6.js
@@ -0,0 +1,2 @@
+let o=document.getElementById("myBtn");window.onscroll=function(){document.body.scrollTop>340||document.documentElement.scrollTop>340?o.style.display="block":o.style.display="none"};
+//# sourceMappingURL=index.806796d6.js.map
